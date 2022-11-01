7ad8785e0cb171d9ac78e51eb8c2f5e1e01c8aee
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-11-01 12:20:11 +0530

    Refactor SpringAopApplication to implement command line runner

--- src/main/java/com/kskr/springaop/SpringAopApplication.java
+++ src/main/java/com/kskr/springaop/SpringAopApplication.java
@@ -1,13 +1,32 @@
 package com.kskr.springaop;
 
+import com.kskr.springaop.business.AnotherBusiness;
+import com.kskr.springaop.business.Business;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAopApplication {
+public class SpringAopApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAopApplication.class, args);
-	}
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @Autowired
+    private Business business;
+
+    @Autowired
+    private AnotherBusiness anotherBusiness;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info(business.calculateSomething());
+        logger.info(anotherBusiness.calculateSomething());
+    }
 }
